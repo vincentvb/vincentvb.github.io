b883a661ee15c3ebdf4955fa2856294c9ecedcb4
Vincent Van Buskirk <vvanbuskirk805@gmail.com>
2022-04-11 09:38:57 -0400

    fix automatic checkpoints

--- ukraine-invasion/config.js
+++ ukraine-invasion/config.js
@@ -13,6 +13,7 @@ var config = {
             hidden: false,
             title: 'February 2014: Crimea is annexed and Russian insurgency begins',
             image: 'assets/ukraine_2014.jpeg',
+            image_description: "Russian troops occupying Crimea",
             description: "Following Ukraine's Euromaiden movement in 2014 that brought into power a pro-western regime, Putin orders the annexation of Crimea. Pro-Russian insurgents in eastern Ukraine are funded and armed by Russia, leading to 8 years of intermittent violence.",
             location: {
                 center: [32.76502, 48.72574],
@@ -43,6 +44,7 @@ var config = {
             hidden: false,
             title: 'February 21, 2022: Putin declares Eastern Ukrainian regions independent, sends in troops',
             image: 'assets/putin_speech.jpeg',
+            image_description: "Vladimir Putin giving live televised address to the world",
             description: "In a televised speech, Putin pronounces the eastern Ukrainian provinces of Donetsk and Luhansk as independent. In the speech Putin questioned Ukraine's legitimacy as a nation, stating that Ukraine has \"never had stable traditions of real statehood.\" He orders troops to occupy the breakaway regions, claiming it necessary for \"peacekeeping operations.\"",
             location: {
                 center: [38.56543, 48.27011],
@@ -78,6 +80,7 @@ var config = {
             hidden: false,
             title: 'February 24, 2022: Putin launches full-scale invasion of Ukraine',
             image: 'assets/kyiv.jpeg',
+            image_description: "Aerial view of Kyiv before the war",
             description: "Three days later, Putin announces a \"special military operation\". Explosions are reported throughout the country as Russian troops move into Ukraine on multiple fronts. The capital, Kyiv, is targeted to the north-west as Russian troops are granted free access through Belarus to launch the attack.",
             location: {
                 center: [30.74184, 49.46828],
@@ -129,6 +132,7 @@ var config = {
             hidden: false,
             title: 'February 24, 2022: Putin launches full-scale invasion of Ukraine',
             image: 'assets/kharkav.jpeg',
+            image_description: "Rubble in Kharkhav following Russian bombardment",
             description: "From the east, Russia strikes at Kharkiv province. The city of Kharkiv itself is heavily shelled, and a massive fire breaks out at the Kharkiv Tractor Plant.",
             location: {
                 center: [36.04093, 49.23503],
@@ -174,6 +178,7 @@ var config = {
             hidden: false,
             title: 'February 24, 2022: Putin launches full-scale invasion of Ukraine',
             image: 'assets/kherson.jpeg',
+            image_description: "A blown up and burned out car among rubble in Kherson",
             description: "Russia uses Crimea as a launching point for a southern offensive into the Kherson province. Within a week, the city of Kherson was surrounded, with parts of the city under full Russian control.",
             location: {
                 center: [33.10341, 46.39235],
@@ -225,6 +230,7 @@ var config = {
             hidden: false,
             title: "February 28, 2022: Russia's initial advance slows and battle lines are drawn",
             image: 'assets/tank.jpeg',
+            image_description: "A Ukrainian tank with Ukrainian soldiers splayed on top of it",
             description: "A week into the invasion, Russia has a military presence in 9 out of Ukraine's 24 provinces. Major cities in the east and south have fallen or are on the cusp of doing so, but Kyiv still stands.",
             location: {
                 center: [33.25827, 48.18265],
